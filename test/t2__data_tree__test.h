43414091d483737b14995687a4580dadc3b49f02
shecannotsee <shecannotsee@163.com>
2023-10-19 16:43:59 +0800

    [test] add data tree test

--- test/t2__data_tree__test.h
+++ test/t2__data_tree__test.h
@@ -5,4 +5,16 @@
 #ifndef DFA_GENERATOR_T2__DATA_TREE__TEST_H
 #define DFA_GENERATOR_T2__DATA_TREE__TEST_H
 
+#include <scanning.h>
+#include <data_tree.h>
+
+TEST(data_tree, process_line) {
+  auto lines = scanning::get_line(DFA_model);
+  data_tree entity;
+  for (const auto& line:lines) {
+    entity.process_line(line);
+  }
+
+}
+
 #endif //DFA_GENERATOR_T2__DATA_TREE__TEST_H
