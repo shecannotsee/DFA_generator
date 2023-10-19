e0fe47ea66fc5615abc7f8cf77042a0dea2cfa3b
shecannotsee <shecannotsee@163.com>
2023-10-19 15:43:36 +0800

    [feat] add framework

---
+++ cmake/DFA_generator-config.cmake
@@ -0,0 +1,13 @@
+
+# ${DFA_generator_FOUND}       : right get
+# ${DFA_generator_INCLUDE_DIR} : include
+# ${DFA_generator_LIBRARIES}   : lib
+# ${DFA_generator_LINK_TARGET} : link target
+
+set(DFA_generator "DFA_generator")
+set(${DFA_generator}_FOUND "1")
+set(${DFA_generator}_INCLUDE_DIR   "${CMAKE_PREFIX_PATH}/include")
+set(${DFA_generator}_LIBRARIES     "${CMAKE_PREFIX_PATH}/lib")
+set(${DFA_generator}_LINK_TARGET   "DFA_generator")
+
+
