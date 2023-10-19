b10095fa71119f9175b8bd34a5b913b6aef91756
shecannotsee <shecannotsee@163.com>
2023-10-19 18:42:09 +0800

    [feat] add application

--- exec/application.cpp
+++ exec/application.cpp
@@ -1,8 +1,22 @@
 // application.cpp
 #include <iostream>
+#include <data_tree.h>
+#include "scanning.h"
 
-int main() {
+int main(int argc, char* argv[]) {
   std::cout << "\033[32m" << "application start" << "\033[0m" << std::endl;
+
+  if (argc==2) {
+    auto lines = scanning::get_line(argv[1]);
+    data_tree entity;
+    for (const auto& line:lines) {
+      entity.process_line(line);
+    }
+
+    std::cout << entity.generate();
+  } else {
+    throw error_message("error input");
+  }
 
   std::cout << "\033[32m" << "application done" << "\033[0m" << std::endl;
   return 0;
